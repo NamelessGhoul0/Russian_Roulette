2392654ad85b41d7ec07fe0fcf6cae25d6be63c6
NamelessGhoul0 <namelessghoul02@gmail.com>
2017-08-12 13:37:06 -0400

    update script

--- clean-repo.sh
+++ clean-repo.sh
@@ -2,8 +2,8 @@
 
 # Just a simple repo cleaner script for the lazy (like me)
 
-rm Makefile cmake_install.cmake CMakeCache.txt
-rm -rf CMakeFiles
+rm Makefile cmake_install.cmake CMakeCache.txt 2> /dev/null
+rm -rf CMakeFiles 2> /dev/null
 
 # Vita specific
-rm *.vpk *.self
+rm *velf *.vpk *elf *.self *suprx *skprx 2> /dev/null
